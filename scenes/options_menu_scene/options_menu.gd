e6bb13131a8e433aaa1dd25d5aadf2cfadc239f0
Joaoherval <89783549+Kennykiller36@users.noreply.github.com>
2026-01-07 21:33:44 -0300

    Commit menu pause e menu de audio

--- scenes/options_menu_scene/options_menu.gd
+++ scenes/options_menu_scene/options_menu.gd
@@ -1,1 +1,33 @@
 extends Control
+
+var audio_bus_master
+var audio_bus_sfx
+var audio_bus_musica
+
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	audio_bus_master= AudioServer.get_bus_index("Master")
+	audio_bus_sfx= AudioServer.get_bus_index("Sfx")
+	audio_bus_musica= AudioServer.get_bus_index("Musica")
+
+
+
+func _on_voltar_menu_btn_pressed():
+	get_tree().change_scene_to_file("res://scenes/main_menu_scene/main_menu.tscn")
+ # Replace with function body.
+
+
+func _on_audio_geral_slider_value_changed(value):
+	var db= linear_to_db(value)
+	AudioServer.set_bus_volume_db(audio_bus_master, db)
+
+
+func _on_audio_sfx_slider_value_changed(value):
+	var db= linear_to_db(value)
+	AudioServer.set_bus_volume_db(audio_bus_sfx, db)
+
+
+
+func _on_audio_musica_slider_value_changed(value):
+	var db= linear_to_db(value)
+	AudioServer.set_bus_volume_db(audio_bus_musica, db)
--- scenes/options_menu_scene/voltar_jogo_btn.gd
+++ scenes/options_menu_scene/voltar_jogo_btn.gd
@@ -1,21 +1,17 @@
 extends Button
 
 @export var gameplay_scene_path := "res://scenes/test_scene/test_scene.tscn"
-# ↑ change this to your actual gameplay scene path
 
 func _ready():
 	_update_visibility()
+	pressed.connect(_on_pressed)
 
 func _process(_delta):
 	_update_visibility()
 
 func _update_visibility():
-	var is_paused = get_tree().paused
-	var current_scene = get_tree().current_scene
+	visible = get_tree().paused
 
-	var is_in_game := (
-		current_scene != null
-		and current_scene.scene_file_path == gameplay_scene_path
-	)
-
-	visible = is_paused and is_in_game
+func _on_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file(gameplay_scene_path)
