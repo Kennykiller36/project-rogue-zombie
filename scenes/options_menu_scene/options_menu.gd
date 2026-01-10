b1cc97468a10d7585698c0af6f9d4d83ce7777cf
Joaoherval <89783549+Kennykiller36@users.noreply.github.com>
2026-01-09 21:06:23 -0300

    Update video tab

--- scenes/options_menu_scene/options_menu.gd
+++ scenes/options_menu_scene/options_menu.gd
@@ -1,9 +1,8 @@
 extends Control
 
-##Parte dos botões da tab geral
 static var opened_from_gameplay: bool = false
-@onready var voltar_jogo_btn = $TabContainer/Geral/VoltarJogoBtn
-@onready var voltar_hub_btn = $TabContainer/Geral/VoltarParaHubBtn
+@onready var voltar_jogo_btn = $TabContainer/Geral/VBoxContainer/VoltarJogoBtn
+@onready var voltar_hub_btn = $TabContainer/Geral/VBoxContainer/VoltarParaHubBtn
 func _update_button_visibility():
 	var should_show = opened_from_gameplay
 	voltar_jogo_btn.visible = should_show
@@ -25,8 +24,49 @@ func _on_voltar_hub_btn_pressed():
 	get_tree().change_scene_to_file("res://scenes/main_menu_scene/main_menu.tscn")
 
 ##Parte da Tab de video
+@onready var janela_button=$TabContainer/Video/VBoxContainerVideoBtn/JanelaOptionBtn as OptionButton
+
+@onready var resolucao_button=$TabContainer/Video/VBoxContainerVideoBtn/ResolucaoOptionBtn as OptionButton
 
 
+const MODO_JANELA_ARRAY: Array[String]=[
+	"Tela cheia", "Janela", "Janela sem borda", "Tela cheia sem borda"
+]
+
+func modo_janela_selecionado(index:int)-> void:
+	match index:
+		0:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+		1:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+
+		2:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+
+		3:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+
+func adicionar_items_modo_janela()-> void:
+	for i in MODO_JANELA_ARRAY:
+		janela_button.add_item(i)
+
+const DICIONARIO_RESOLUCAO:Dictionary={
+	"1152 x 648": Vector2i(1152,648),
+	"1280 x 720": Vector2i(1280,720),
+	"1920 x 1080": Vector2i(1920,1080),
+	"1920 x 1200": Vector2i(1920,1200)
+}
+func adicionar_items_resolucao()-> void:
+	for j in DICIONARIO_RESOLUCAO:
+		resolucao_button.add_item(j)
+		
+func resolucao_selecionada(index:int)-> void:
+	DisplayServer.window_set_size(DICIONARIO_RESOLUCAO.values()[index])
+	
 ##Parte da Tab de audio
 var audio_bus_master
 var audio_bus_sfx
@@ -46,9 +86,17 @@ func _on_audio_musica_slider_value_changed(value):
 
 ##Parte da função ready
 func _ready():
+	##Parte da tab geral 2
 	process_mode = Node.PROCESS_MODE_ALWAYS
+	_update_button_visibility()
+	
+	##Parte da tab audio 2
 	audio_bus_master= AudioServer.get_bus_index("Master")
 	audio_bus_sfx= AudioServer.get_bus_index("Sfx")
 	audio_bus_musica= AudioServer.get_bus_index("Musica")
 	
-	_update_button_visibility()
+	##Parte da tab video 2
+	janela_button.item_selected.connect(modo_janela_selecionado)
+	adicionar_items_modo_janela()
+	resolucao_button.item_selected.connect(resolucao_selecionada)
+	adicionar_items_resolucao()
