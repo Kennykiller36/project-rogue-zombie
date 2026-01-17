0501fd3c4685f6650f5718821ead8ef1817627cd
Joaoherval <89783549+Kennykiller36@users.noreply.github.com>
2026-01-16 21:04:38 -0300

    add sistema de munição

--- entidades/arma/arma.gd
+++ entidades/arma/arma.gd
@@ -1,22 +1,35 @@
 extends Node2D
 
-const BALA=preload("res://entidades/bala/bullet.tscn")
+const BALA = preload("res://entidades/bala/bullet.tscn")
 
-# Called when the node enters the scene tree for the first time.
+@export var infinite_ammo: bool = false
+@export var max_ammo: int = 10
+var current_ammo: int
+
 func _ready() -> void:
-	pass # Replace with function body.
+	current_ammo = max_ammo
 
+func atira() -> void:
+	# Stop firing if out of ammo and not infinite
+	if !infinite_ammo and current_ammo <= 0:
+		return
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	var bullet_instance = BALA.instantiate()
+	get_tree().root.add_child(bullet_instance)
+	bullet_instance.global_position = global_position
+	bullet_instance.rotation = rotation
+
+	if !infinite_ammo:
+		current_ammo -= 1
+
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
-	rotation_degrees=wrap(rotation_degrees,0,360)
-	if rotation_degrees>90 and rotation_degrees<270:
-		scale.y=-1
+	rotation_degrees = wrap(rotation_degrees, 0, 360)
+
+	if rotation_degrees > 90 and rotation_degrees < 270:
+		scale.y = -1
 	else:
-		scale.y=1
+		scale.y = 1
+
 	if Input.is_action_just_pressed("fire"):
-		var bullet_instance=BALA.instantiate()
-		get_tree().root.add_child(bullet_instance)
-		bullet_instance.global_position=global_position
-		bullet_instance.rotation=rotation
+		atira()
