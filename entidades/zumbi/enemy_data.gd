0728f0fdd2c674930b2163f472bb2d5c968b7a5a
Joaoherval <89783549+Kennykiller36@users.noreply.github.com>
2026-01-22 13:56:12 -0300

    add resource para o zumbi

---
+++ entidades/zumbi/enemy_data.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name EnemyData
+
+@export var vidamaxima: int = 10
+@export var dano: int = 1
+@export var sprite_texture: Texture2D
+@export var velocidade: float = 50.0
--- entidades/zumbi/zumbi.gd
+++ entidades/zumbi/zumbi.gd
@@ -1,46 +1,62 @@
 extends CharacterBody2D
 
-@export var dano: int
-@export var saude: int
-const speed := 20
-
+@export var enemy_data: EnemyData
+# Local (runtime) stats
 var player: Node2D
+var current_health: int
+var damage: int
+var speed: float
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
+@onready var sprite_component := $Sprite as Sprite2D
 
-## Vida e dano do inimigo
-func diminuir_vida(dano: int) -> void:
-	if saude <= 0:
+
+func diminuir_vida(dano_recebido: int) -> void:
+	current_health -= dano_recebido
+	if current_health <= 0:
 		queue_free()
-		return
-	saude -= dano
+
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
 		var player_node := area.get_parent()
 		if player_node.has_method("diminuir_vida"):
-			player_node.diminuir_vida(dano)
+			player_node.diminuir_vida(damage)
+
+func makePath() -> void:
+	if player:
+		nav_agent.target_position = player.global_position
+
+
+func _on_timer_timeout() -> void:
+	makePath()
+
+func find_player() -> void:
+	player = get_tree().get_first_node_in_group("player") as Node2D
+	if not player:
+		push_warning(
+			"Enemy spawned but no player found in group '" + enemy_data.player + "'"
+		)
 
 ## Navegação
 func _physics_process(delta: float) -> void:
-	if not player:
+	if not player or nav_agent.is_navigation_finished():
 		return
 
 	var dir := to_local(nav_agent.get_next_path_position()).normalized()
 	velocity = dir * speed
 	move_and_slide()
 
-func makePath() -> void:
-	if player:
-		nav_agent.target_position = player.global_position
-
-func _on_timer_timeout() -> void:
-	makePath()
-
 func _ready() -> void:
+	if not enemy_data:
+		push_error("Enemy spawned without EnemyData!")
+		return
+
+	current_health = enemy_data.vidamaxima
+	damage = enemy_data.dano
+	speed = enemy_data.velocidade
+
+	if enemy_data.sprite_texture:
+		sprite_component.texture = enemy_data.sprite_texture
+
 	find_player()
 	makePath()
-
-func find_player() -> void:
-	player = get_tree().get_first_node_in_group("player") as Node2D
-	if not player:
-		push_warning("Enemy spawned but no player found in group 'player'")
